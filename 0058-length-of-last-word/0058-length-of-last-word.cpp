21cd16baadaf2f143ba90030f114f0463f1b305c
Namandeep Singh <naman.deep991@gmail.com>
2023-08-22 00:02:58 +0530

    Time: 4 ms (33.08%), Space: 6.5 MB (39.51%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int i, j;
+        for(i=s.size()-1; i>=0; i--) {
+            if(s[i] != ' ') {
+                break;
+            }
+        }
+        for(j=i; j>=0; j--) {
+            if(s[j] == ' ') {
+                break;
+            }
+        }
+        return i - j;
+    }
+};
