a2fe30fc659e77cd53873735aa68ea5cb335c8d5
Namandeep Singh <naman.deep991@gmail.com>
2024-09-21 17:42:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Introduction to Trees/introduction-to-trees.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+// } Driver Code Ends
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int countNodes(int i) {
+        if(i==1) return 1;
+        if(i==0) return 0;
+        return pow(2, i)/2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int i;
+        cin >> i;
+        Solution ob;
+        int res = ob.countNodes(i);
+
+        cout << res;
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
